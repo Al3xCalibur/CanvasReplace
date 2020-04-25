5dc1e5a51d72a3635fb3f2143948d3ceff6456f6
Ardhanaariishvara <5360806-Ardhanaariishvara@users.noreply.gitlab.com>
2020-04-25 02:05:01 +0200

    résolution (enfin?) du scroll-locking

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -135,7 +135,7 @@ function scroll(e) {
         translateY -= y * (1 / scale - 1 / previous)
 
         let min_propor = size * showPercent / scale
-        let max_propor = size * (1 - showPercent) / scale
+        let max_propor = size * (1 + 1 / scale * showPercent)
 
         if (translateX < -width * min_propor) translateX = -width * min_propor
         if (translateX > width * max_propor) translateX = width * max_propor
@@ -172,8 +172,11 @@ function moveCanvas(e) {    //legers pb avec startDrag : on dessine pas toujours
 
     if (startDrag == null) return startDrag = new Vector(e.x, e.y)
 
-    if (translateX - e.movementX / scale > -width * size / scale * showPercent && translateX - e.movementX / scale < width * size * (1 + 1 / scale * showPercent) - ctx.canvas.width / scale &&
-        translateY - e.movementY / scale > -width * size / scale * showPercent && translateY - e.movementY / scale < height * size * (1 + 1 / scale * showPercent) - ctx.canvas.height / scale &&
+    let min_propor = size * showPercent / scale
+    let max_propor = size * (1 + 1 / scale * showPercent)
+
+    if (translateX - e.movementX / scale > -width * min_propor && translateX - e.movementX / scale < width * max_propor - ctx.canvas.width / scale &&
+        translateY - e.movementY / scale > -height * min_propor && translateY - e.movementY / scale < height * max_propor - ctx.canvas.height / scale &&
         startDrag.add(new Vector(-e.x, -e.y)).magnitude() > 10
     ) {
         translateX -= e.movementX / scale
